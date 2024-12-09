7a66cb7a4eec8c56e23dd6927cb4a6d3dadf2c7f
Nicole Mervine <nicole.mervine@gmail.com>
2024-12-09 08:57:57 -0500

    Days 8 and 9

---
+++ Day 8/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var debug = false;
+//debug = true;
+string path = debug ? "Test.txt" : "Input.txt";
+var input = System.IO.File.ReadAllLines(path).ToList();
+
+//Part 1
+
+var points = new Dictionary<Point, char>();
+var maxX = input[0].Length;
+var maxY = input.Count;
+
+for (int y = 0; y < maxY; y++)
+    for (int x = 0; x < maxX; x++)
+        if (input[y][x] != '.')
+            points.Add(new Point(x, y), input[y][x]);
+
+var antinodes = new Dictionary<char, List<Point>>();
+var sw = Stopwatch.StartNew();
+
+foreach (var antenna in points.Values.Distinct())
+{
+    var antennas = points.Where(x => x.Value == antenna).Select(x => x.Key).ToList();
+    var nodeList = new List<Point>();
+    foreach (var a in antennas)
+    {
+        var tempList = antennas.Where(x => x != a).ToList();
+        foreach (var temp in tempList)
+        {
+            var tempNodes = GetAntinodes(a, temp,maxX,maxY);
+            if (tempNodes.Count >= 2)
+                nodeList.Add(tempNodes[1]);
+        }
+    }
+    antinodes.Add(antenna, nodeList);
+}
+
+var nodes = new List<Point>();
+foreach (var item in antinodes.Values)
+{
+    nodes.AddRange(item);
+}
+
+Console.WriteLine($"Part 1 ({sw.ElapsedTicks / 10000.0}ms): {nodes.Distinct().Count()}");
+
+//Part 2
+
+sw.Restart();
+antinodes = new Dictionary<char, List<Point>>();
+
+foreach (var antenna in points.Values.Distinct())
+{
+    var antennas = points.Where(x => x.Value == antenna).Select(x => x.Key).ToList();
+    var nodeList = new List<Point>();
+    foreach (var a in antennas)
+    {
+        var tempList = antennas.Where(x => x != a).ToList();
+        foreach (var temp in tempList)
+        {
+            nodeList.AddRange(GetAntinodes(a, temp,maxX,maxY));
+        }
+    }
+    antinodes.Add(antenna, nodeList);
+}
+
+nodes = new List<Point>();
+foreach (var item in antinodes.Values)
+{
+    nodes.AddRange(item);
+}
+
+Console.WriteLine($"Part 2 ({sw.ElapsedTicks/10000.0}ms): {nodes.Distinct().Count()}");
+
+Point GetAntinode(Point p1, Point p2)
+{
+    var antinode = new Point();
+    if (p1.X < p2.X)
+        antinode.X = p1.X - (p2.X - p1.X);
+    else if (p1.X > p2.X)
+        antinode.X = p1.X + (p1.X - p2.X);
+    else
+        antinode.X = p1.X;
+    if (p1.Y < p2.Y)
+        antinode.Y = p1.Y - (p2.Y - p1.Y);
+    else if (p1.Y > p2.Y)
+        antinode.Y = p1.Y + (p1.Y - p2.Y);
+    else
+        antinode.Y = p1.Y;
+    return antinode;
+}
+
+List<Point> GetAntinodes(Point p1, Point p2, int maxX, int maxY)
+{
+    var nodeList = new List<Point>();
+    nodeList.Add(p1);
+    
+    var currentNode = GetAntinode(p1,p2);
+    while ((0 <= currentNode.X) && (0 <= currentNode.Y) && (currentNode.X < maxX) && (currentNode.Y < maxY))
+    {
+        nodeList.Add(new Point(currentNode.X, currentNode.Y));
+        currentNode = GetAntinode(nodeList.Last(), nodeList[nodeList.Count-2]);
+    }
+
+    return nodeList;
+}
---
+++ Day 9/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var debug = false;
+//debug = true;
+string path = debug ? "Test.txt" : "Input.txt";
+var input = System.IO.File.ReadAllText(path).Select(x => Int32.Parse(x.ToString())).ToList();
+
+//Part 1
+
+var sw = Stopwatch.StartNew();
+var id = 0;
+var fileblocks = new Dictionary<int, string>();
+long sum = 0;
+
+for (int i = 0; i < input.Count; i++)
+{
+    for (int j = 0; j < input[i]; j++)
+    {
+        if (i % 2 == 0)
+            fileblocks.Add(fileblocks.Count, id.ToString());
+        else
+            fileblocks.Add(fileblocks.Count, ".");
+    }
+    if (i % 2 == 0)
+        id++;
+}
+
+for (int i = 0; i < fileblocks.Count; i++)
+    if (fileblocks.Where(x => x.Key >= i).All(x => x.Value == "."))
+        break;
+    else if (fileblocks[i] == ".")
+    {
+        var tempBlock = fileblocks.Where(x => x.Value != ".").Last();
+        fileblocks[i] = new string(tempBlock.Value);
+        fileblocks[tempBlock.Key] = ".";
+    }
+
+
+foreach (var block in fileblocks.Where(x => x.Value != "."))
+    sum += block.Key * Convert.ToInt32(block.Value);
+
+Console.WriteLine($"Part 1 ({sw.ElapsedMilliseconds}ms): {sum}");
+
+//Part 2
+
+sw.Restart();
+
+id = 0;
+var filesystem = "";
+sum = 0;
+
+for (int i = 0; i < input.Count; i++)
+{
+    if (i % 2 == 0)
+    {
+        filesystem += string.Concat(Enumerable.Repeat($"f{id}f", input[i]));
+        id++;
+    }
+    else
+        filesystem += string.Concat(Enumerable.Repeat(".", input[i]));
+}
+id--;
+
+for (int i = id; i > 0; i--)
+{
+    //Console.WriteLine(filesystem);
+    string pattern = $"(f{i}f)+";
+    var match = Regex.Match(filesystem, pattern);
+    var matches = Regex.Matches(filesystem, pattern.Replace("+",""));
+    var length = matches.Count;
+    var openSpot = Regex.Match(filesystem.Substring(0, match.Index), $"\\.{{{length}}}");
+    if (openSpot.Success)
+    {
+        filesystem = filesystem.Remove(match.Index,match.Length);
+        filesystem = filesystem.Insert(match.Index, string.Concat(Enumerable.Repeat(".", length)));
+        filesystem = filesystem.Remove(openSpot.Index, openSpot.Length);
+        filesystem = filesystem.Insert(openSpot.Index, match.Value);
+    }
+}
+
+int index = 0;
+while (filesystem.Length > 0)
+{
+    if (filesystem[0] != '.')
+    {
+        var match = Regex.Match(filesystem, $"^f\\d+f");
+        sum += index * Convert.ToInt32(match.Value.Replace("f", ""));
+        filesystem = filesystem.Remove(0, match.Length);
+    }
+    else
+        filesystem = filesystem.Remove(0, 1);
+    index++;
+}
+
+Console.WriteLine($"Part 2 ({sw.ElapsedMilliseconds}ms): {sum}");
